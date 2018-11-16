e01843b07737c93495bbd152968152ec285880ee
longduer <18321972003@163.com>
2018-11-16 15:00:50 +0800

    rest to upload

--- listener.js
+++ listener.js
@@ -18,7 +18,7 @@ var source = fs.readFileSync('./contracts/sait.sol', 'utf8');
 var compiledContract = solc.compile(source, 1);
 
 var abi = compiledContract.contracts[':SAIToken'].interface;
-// console.info('abi: ' + JSON.stringify(abi));
+console.info('abi: ' + JSON.stringify(abi));
 
 // var bytecode = compiledContract.contracts[':SAIToken'].bytecode;
 // console.info('bytecode: ' + bytecode);
@@ -38,29 +38,29 @@ var myContractInstance = MyContract.at('0x34f0d846c766874413938994da32360Cf0E435
 //
 // console.info('address balance: ' + web3.fromWei(balance, 'ether'));
 
-var locked = true;
-
-function scheduleCronstyle() {
-
-    //job任务
-    var j = schedule.scheduleJob('0 */3 * * * ?', function () {
-        locked = myContractInstance.frozenAccount('0x79E3FaA828793b8d96E947f59DFA4b767e12B5Ed');
-        console.info(new Date() + ' sait 合约 -- locked: ' + locked);
-        if (!locked) {
-            sms.sendSms('18321972003', '1200');
-        }
-    });
-
-    setInterval(function () {
-        console.info(new Date() + ' -- setInterval 取消定时检查 -- locked: ' + locked);
-        if (!locked) {
-            console.log(new Date() + ' -- locked: ' + locked + '定时器取消');
-            j.cancel();
-        }
-    },  1*60*1000); //每一分钟检查一下，进行取消操作
-}
-
-scheduleCronstyle();
+// var locked = true;
+//
+// function scheduleCronstyle() {
+//
+//     //job任务
+//     var j = schedule.scheduleJob('0 */3 * * * ?', function () {
+//         locked = myContractInstance.frozenAccount('0x79E3FaA828793b8d96E947f59DFA4b767e12B5Ed');
+//         console.info(new Date() + ' sait 合约 -- locked: ' + locked);
+//         if (!locked) {
+//             sms.sendSms('18321972003', '1200');
+//         }
+//     });
+//
+//     setInterval(function () {
+//         console.info(new Date() + ' -- setInterval 取消定时检查 -- locked: ' + locked);
+//         if (!locked) {
+//             console.log(new Date() + ' -- locked: ' + locked + '定时器取消');
+//             j.cancel();
+//         }
+//     },  1*60*1000); //每一分钟检查一下，进行取消操作
+// }
+//
+// scheduleCronstyle();
 
 
 //myContractInstance.freezeAccount('0x79E3FaA828793b8d96E947f59DFA4b767e12B5Ed', false);
